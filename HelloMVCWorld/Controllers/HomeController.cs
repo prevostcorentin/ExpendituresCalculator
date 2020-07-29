260e50c573651b66ee30ffc5b2322f5bf5f8e206
Corentin PREVOST <corentin.prevost@wildcodeschool.fr>
2020-07-29 10:52:07 +0200

    Initial commit

---
+++ HelloMVCWorld/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloMVCWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Controllers.HelloMVCWorld
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var spent = new List<Spent> {
+                new Spent { Amount = 10, Name = "Random" },
+                new Spent { Amount = 10, Name = "Random" },
+                new Spent { Amount = 10, Name = "Random" },
+                new Spent { Amount = 10, Name = "Random" },
+            };
+            return View(spent);
+        }
+    }
+}
---
+++ HelloMVCWorld/Models/Spent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloMVCWorld.Models
+{
+    public class Spent
+    {
+        private List<Spent> _subSpents;
+        public Double Amount { get; set; }
+        public String Name { get; set; }
+    }
+}
