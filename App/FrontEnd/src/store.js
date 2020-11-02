cfe250ee95d2d493200acffc6a7f51f1c38541eb
Corentin PREVOST <corentin.prevost@wildcodeschool.fr>
2020-11-02 19:34:34 +0100

    Spent to expenditures

--- App/FrontEnd/src/store.js
+++ App/FrontEnd/src/store.js
@@ -8,9 +8,23 @@ export default new Vuex.Store({
   state: {
     expenditures: [],
   },
+  actions: {
+    filterExpenditures({ commit }, filter = []) {
+      commit('postExpenditures', filter)
+    },
+    pushExpenditure({ state }, expenditure) {
+      state.expenditures.push(expenditure)
+    },
+    saveExpenditure({ commit }, expenditure) {
+      commit('putExpenditure', expenditure)
+    },
+    eraseExpenditure({ commit }, expenditure) {
+      commit('deleteExpenditure', expenditure)
+    }
+  },
   mutations: {
     postExpenditures(state, filter) {
-      Axios.post(`${process.env.BASE_URL}/spent`, filter, {
+      Axios.post(`${process.env.BASE_URL}/expenditure`, filter, {
         headers: {
           'Content-Type': 'application/json'
         }
@@ -19,32 +33,18 @@ export default new Vuex.Store({
       })
     },
     putExpenditure (state, expenditure) {
-      Axios.put(`${process.env.BASE_URL}/spent`, expenditure, {
+      Axios.put(`${process.env.BASE_URL}/expenditure`, expenditure, {
         headers: {
           'Content-Type': 'application/json'
         }
       })
     },
     deleteExpenditure(state, expenditure) {
-      Axios.delete(`${process.env.BASE_URL}/spent/`, expenditure, {
+      Axios.delete(`${process.env.BASE_URL}/expenditure/`, expenditure, {
         headers: {
           'Content-Type': 'application/json'
         }
       })
-    }
-  },
-  actions: {
-    filterExpenditures ({ commit }, filter = []) {
-      commit('postExpenditures', filter)
-    },
-    pushExpenditure({ commit, state }, expenditure) {
-      state.expenditures.push(expenditure)
-    },
-    saveExpenditure({ commit }, expenditure) {
-      commit('putExpenditure', expenditure)
-    },
-    eraseExpenditure({ commit }, expenditure) {
-      commit('deleteExpenditure', expenditure)
     }
   }
 })
